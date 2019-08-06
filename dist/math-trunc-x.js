9ffd23c1a192c987bce024a12d71a25a7d251b17
Graham Fairweather <xotic750@gmail.com>
2019-08-06 02:31:00 +0200

    :bookmark: v1.0.10
    Coveralls

--- dist/math-trunc-x.js
+++ dist/math-trunc-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-04T19:30:52.263Z",
+  "date": "2019-08-06T00:30:53.268Z",
   "describe": "",
   "description": "Get the integer part of a number.",
   "file": "math-trunc-x.js",
