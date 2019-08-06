91f20b2206816981ec12067f362b62cd6d795b39
Graham Fairweather <xotic750@gmail.com>
2019-08-06 22:57:45 +0200

    Add coveralls badge

--- dist/math-trunc-x.js
+++ dist/math-trunc-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2019-present",
-  "date": "2019-08-06T00:30:53.268Z",
+  "date": "2019-08-06T20:57:39.430Z",
   "describe": "",
   "description": "Get the integer part of a number.",
   "file": "math-trunc-x.js",
